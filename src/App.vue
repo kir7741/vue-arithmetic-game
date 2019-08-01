fa2021bf75accca1d074ea2250eb29458b53f387
Joseph Lin <joseph@xtremeapp.com.tw>
2019-08-01 14:05:14 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <Layout />
+  </div>
+</template>
+
+<script>
+import Layout from './components/Layout.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Layout
+  }
+}
+</script>
+
+<style lang="scss">
+* {
+  box-sizing: border-box;
+}
+body {
+  margin: 0;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ src/components/Layout.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="wrapper">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Layout'
+}
+</script>
+
+<style scoped lang="scss">
+.wrapper {
+  height: 100vh;
+  min-height: 600px;
+}
+
+</style>
+
+
+
